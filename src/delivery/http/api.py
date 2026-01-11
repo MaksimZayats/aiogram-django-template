332bbad26a34278ce4a9b0b69bc336711b8cfd0c
Maksim Zayats <maximzayats1@gmail.com>
2026-01-11 08:45:13 +0400

    refactor: update environment configuration and improve Docker setup for better clarity

--- src/delivery/http/api.py
+++ src/delivery/http/api.py
@@ -4,9 +4,11 @@ from django.contrib.admin.views.decorators import staff_member_required
 from ninja import NinjaAPI, Router
 from punq import Container
 
+from core.configs.settings import application_settings
 from delivery.http.health.controllers import HealthController
 from delivery.http.user.controllers import UserController, UserTokenController
 from infrastructure.delivery.controllers import Controller
+from infrastructure.settings.types import Environment
 from ioc.container import get_container
 
 
@@ -17,9 +19,14 @@ def get_ninja_api(
 ) -> NinjaAPI:
     container = container or get_container()
 
+    if application_settings.env == Environment.PRODUCTION:
+        docs_decorator = staff_member_required
+    else:
+        docs_decorator = None
+
     ninja_api = NinjaAPI(
         urls_namespace=urls_namespace,
-        docs_decorator=staff_member_required,
+        docs_decorator=docs_decorator,
     )
 
     health_router = Router(tags=["health"])
